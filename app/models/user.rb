d58d7a9293a3052f9ba08143e23acf5a29fd1d75
EC2 Default User <ec2-user@ip-172-31-46-162.ap-northeast-1.compute.internal>
2023-12-29 06:23:14 +0000

    Userモデルの作成 (1)
    ※ マイグレーション前

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  attachment :profile_image
+
+  validates :username, uniqueness: true, length: { in: 4..20 }, format: {
+    with: /\A[a-z0-9_]+\z/i,
+    message: 'は英数字またはアンダーバー(_)のみが使用可能です。'
+  }
+  validates :display_name, length: { in: 1..50 }
+  validates :introduction, length: { in: 0..100 }
+end
