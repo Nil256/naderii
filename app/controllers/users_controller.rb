7bc931799dac0154ff5e85931a10b24db0e72aa5
EC2 Default User <ec2-user@ip-172-31-46-162.ap-northeast-1.compute.internal>
2023-12-30 05:06:31 +0000

    usersコントローラにupdateアクションを追加 それに伴うルートの追加とビューの変更と追加
    ユーザーの更新を機能するようにした。また、一部の設定はdangerzoneとしてルートとアクション、ビューを別に追加した。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,52 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find_by(username: params["username"])
+    @user = User.find_by!(username: params["username"])
   end
 
   def edit
-    @user = User.find_by(username: params["username"])
+    @user = User.find_by!(username: params["username"])
+    if !(user_signed_in?) || current_user.id != @user.id
+      redirect_to user_path(@user.username), danger: "他のユーザーのプロフィールは編集できません。"
+    end
+  end
+
+  def dangeredit
+    @user = User.find_by!(username: params["username"])
+    if !(user_signed_in?) || current_user.id != @user.id
+      redirect_to user_path(@user.username), danger: "他のユーザーのプロフィールは編集できません。"
+    end
+  end
+
+  def update
+    @user = User.find_by!(username: params["username"])
+    if !(user_signed_in?) || current_user.id != @user.id
+      redirect_to user_path(@user.username), danger: "他のユーザーのプロフィールは編集できません。"
+    end
+    if @user.update(user_params)
+      redirect_to user_path(@user.username), success: "プロフィールを更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  def dangerupdate
+    @user = User.find_by!(username: params["username"])
+    if !(user_signed_in?) || current_user.id != @user.id
+      redirect_to user_path(@user.username), danger: "他のユーザーのプロフィールは編集できません。"
+    end
+    if @user.update(user_params_danger)
+      redirect_to user_path(@user.username), success: "プロフィールを更新しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:profile_image, :display_name, :introduction)
+  end
+
+  def user_params_danger
+    params.require(:user).permit(:username)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
 
+  get "/users/@:username/edit/dangerzone" => "users#dangeredit", as: "dangeredit_user"
   get "/users/@:username/edit" => "users#edit", as: "edit_user"
   get "/users/@:username" => "users#show", as: "user"
+  patch "/users/@:username/danger" => "users#dangerupdate"
+  put "/users/@:username/danger" => "users#dangerupdate"
+  patch "/users/@:username" => "users#update"
+  put "/users/@:username" => "users#update"
 end
