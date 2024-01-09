fa79bce3a5d48a46c94a0ccb2f91de14bbf837bb
EC2 Default User <ec2-user@ip-172-31-46-162.ap-northeast-1.compute.internal>
2024-01-09 13:12:33 +0000

    ログアウトの遷移先指定/タイムライン作成のautofocus変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(current_user.username)
   end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end
